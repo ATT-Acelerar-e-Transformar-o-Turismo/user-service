bed190b7783f74f2a450d07ec88f12a19d352455
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2025-11-12 15:55:24 +0000

    Add CORS support and Docker hot-reload with dotenv

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+# CORS Configuration
+origins = os.getenv("ORIGINS", "localhost").split(",")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
